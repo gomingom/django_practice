8718cf27122bf9290ddab84e9e23982779bb479b
gomingom <yeongh.kim17@gmail.com>
2024-06-24 20:57:35 +0900

    refactoring page view using template

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASIS_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THRIDPATY_APPS = [
+    'users',
+]
+
+INSTALLED_APPS = BASIS_APPS + THRIDPATY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/views.py
+++ users/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse(request, 'home.html')
+    return render(request, 'home.html')
