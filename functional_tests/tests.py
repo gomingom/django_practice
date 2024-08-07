672dafc17c95ef6323a93f9136400d75c1b90e82
gomingom <yeongh.kim17@gmail.com>
2024-08-07 21:24:57 +0900

    minimum design for execute

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -21,14 +21,16 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
 
-        self.assertIn("To-Do", self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
-        self.assertIn("To-Do", header_text)
+        # self.assertIn("To-Do", self.browser.title)
+        # header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        # self.assertIn("To-Do", header_text)
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1. 공작깃털 사기" 아이템이 추가된다
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("공작깃털 사기")
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
         self.check_for_row_in_list_table("1: 공작깃털 사기")
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
@@ -38,6 +40,36 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("공작깃털을 이용해서 그물 만들기")
         inputbox.send_keys(Keys.ENTER)
 
+        # 페이지가 다시 갱신되고, 두 개 아이템이 목록에 보인다
         self.check_for_row_in_list_table("2: 공작깃털을 이용해서 그물 만들기")
         self.check_for_row_in_list_table("1: 공작깃털 사기")
-        self.fail("Finish the test!")
+
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+
+        ## 새로운 브라우저 세션을 이용해서 에디슨의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 프란시스가 홈페이지에 접속한다.
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("공작깃털 사기", page_text)
+        self.assertNotIn("그물 만들기", page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작했다
+        # 그는 에디스보다 재미가 없다
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("우유 사기")
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인 한다
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("공작깃털 사기", page_text)
+        self.assertIn("우유 사기", page_text)
--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists")
+        return redirect("")
 
     items = Item.objects.all()
 
