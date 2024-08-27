2c402d179bc146661b89677453af823f648e25c7
gomingom <yeongh.kim17@gmail.com>
2024-08-27 21:15:41 +0900

    new URL and view for adding item in list. Success FT

--- config/urls.py
+++ config/urls.py
@@ -22,9 +22,10 @@ from lists import views
 urlpatterns = [
     re_path(r"^$", views.home_page, name="home"),
     re_path(
-        r"^lists/the-only-list-in-the-world/$",
+        r"^lists/(\d+)/$",
         views.view_list,
         name="view_list",
     ),
+    re_path(r"^lists/(\d+)/add_item$", views.add_item, name="add_item"),
     re_path(r"^lists/new$", views.new_list, name="new_list"),
 ]
